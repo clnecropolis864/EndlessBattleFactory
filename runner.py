193f6b46c2d28195b1490ca3a2fcdaaf432039a0
clnecropolis864 <clnecropolis864@gmail.com>
2018-02-04 12:51:06 -0500

    THE MENU SCREEN WORKS

--- runner.py
+++ runner.py
@@ -5,37 +5,81 @@ from pokemon import Pokemon
 import copy
 import pygame as pg
 import sys
-import graphicsClasses
+from textbox import TextInput
 
+#Initializers
 pg.init()
 pg.font.init()
+
+#Screen
 size = width, height = 640, 480
 screen = pg.display.set_mode(size)
+
+#Misc
 pg.display.set_caption("EndlessBattleFactory")
+textinput = TextInput()
+clock = pg.time.Clock()
+name = ""
 
-while True:
-	active = False
-	nameInput = pg.Rect(100, 100, 140, 32)
-	for event in pg.event.get():
 
-		if event.type == pg.QUIT:
-			# Close window and exit program
-			sys.exit()
+def menu():
+	pokemonSelected = 0
 
-	rect = pg.draw.rect(screen, (100, 100, 100), (50, 50, 50, 50))
-	pg.display.flip()
+	while True:
+		#Preliminaries 
+		screen.fill((225, 225, 225))
+		font = pg.font.SysFont('Roboto', 30)
+		events = pg.event.get()
 
-	font = pg.font.SysFont('Roboto', 30)
-	pickPokemon = font.render('Pick a pokemon:', False, (100, 100, 100))
-	screen.blit(pickPokemon, (0, 0))
-	pg.display.update()
+		for event in events:
+			if event.type == pg.QUIT:
+				# Close window and exit program
+				sys.exit()
 
+		# Feed it with events every frame
+		textinput.update(events)
+		# Blit its surface onto the screen
+		screen.blit(textinput.get_surface(), (0, 50))
+
+		#Test Box
+		"""
+		rect = pg.draw.rect(screen, (100, 100, 100), (50, 50, 50, 50))
+		pg.display.flip()
+		"""
+
+		#Enter name
+		enterName = font.render('Enter your name:', False, (100, 100, 100))
+		screen.blit(enterName, (0, 0))
+
+		if textinput.enter():
+			print (textinput.get_text())
+			name = textinput.get_text()
+
+		#"Pick a pokemon"
+		pickPokemon = font.render('Pick a pokemon:', False, (100, 100, 100))
+		screen.blit(pickPokemon, (0, 100))
+
+		#Sprites
+		venuS = pg.image.load("sprites/003.png")
+		screen.blit(venuS, (0, 150))
+		chariS = pg.image.load("sprites/006.png")
+		screen.blit(chariS, (venuS.get_width(), 150))
+		blastS = pg.image.load("sprites/009.png")
+		screen.blit(blastS, (venuS.get_width() + chariS.get_width(), 150))
+
+		pg.display.update()
+
+		if pokemonSelected > 2:
+			break
+
+		clock.tick(60)
+
+def start():
 	Charizard = dex.charizard()
 	Venusaur = dex.venusaur()
 	Blastoise = dex.blastoise()
 
 	party = [dex.charizard(), dex.venusaur(), dex.blastoise()]
-	name = "test"
 	you = trainer.Trainer(name, party)
 
 	oppName = "John"
@@ -44,5 +88,10 @@ while True:
 
 
 	battle.run(you, opp)
+
+
+menu()
+start()
+
 
 
--- tester.py
+++ tester.py
@@ -1,3 +1,4 @@
+"""
 import calc
 import trainer
 import pokedex as dex
@@ -8,3 +9,30 @@ modifier = [1, 1, 1, 1.5, 2, 1, 1]
 party = [dex.charizard(), dex.venusaur(), dex.blastoise()]
 you = trainer.Trainer("Chuck", party)
 print(type(you))
+"""
+import textbox
+import pygame
+pygame.init()
+
+# Create TextInput-object
+textinput = textbox.TextInput()
+
+screen = pygame.display.set_mode((1000, 200))
+clock = pygame.time.Clock()
+
+while True:
+    screen.fill((225, 225, 225))
+
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            exit()
+
+    # Feed it with events every frame
+    textinput.update(events)
+    # Blit its surface onto the screen
+    screen.blit(textinput.get_surface(), (10, 10))
+
+    pygame.display.update()
+    clock.tick(30)
+
